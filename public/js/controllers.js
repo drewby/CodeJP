a328472f74445f57cd173b50453282341c8ca2a0
drew@drewby.com <drew@drewby.com>
2014-08-10 07:40:58 +0000

    fixed typescript references

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,10 +1,9 @@
-/// <reference path="../typings/angularjs/angular.d.ts" />
-/// <reference path="../typings/angularjs/angular-resource.d.ts" />
+/// <reference path="../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../typings/angularjs/angular-resource.d.ts" />
 
 'use strict';
 
 /* Controllers */
-
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1',  ['$scope', '$http', '$resource', function($scope, $http, $resource) {
       $scope.people = $resource('/api/people').query();
